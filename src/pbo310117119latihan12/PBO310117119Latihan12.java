31b0a2e7824089a4b8226f746a8e77a15f08b200
ARDITYAYS <220499yogaputa@gmail.com>
2018-09-23 21:26:48 +0700

    first commit

---
+++ src/pbo310117119latihan12/PBO310117119Latihan12.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo310117119latihan12;
+
+/**
+ *
+ * @author  
+ * Nama      : Arditya Yogaputra S
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini menampilkan Aritmatika
+ */
+public class PBO310117119Latihan12 {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = "+(a+b));
+        System.out.println("a - b = "+(a-b));
+        System.out.println("a * b = "+(a*b));
+        System.out.println("b / a = "+(b/a));
+        System.out.println("b % a = "+(b%a));
+        System.out.println("c % a = "+(c%a));
+        System.out.println("a++ = "+(a++));
+        System.out.println("b-- = "+(b--));
+        System.out.println("d++ = "+(d++));
+        System.out.println("++d = "+(++d));
+        
+    }
+    
+}
